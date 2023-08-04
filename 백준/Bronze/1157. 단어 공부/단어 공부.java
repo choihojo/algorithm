07fe0726516ceedb421a6594b42cdde9e2aa8893
Hojo Choi <87483951+ubermensch100326@users.noreply.github.com>
2023-08-04 15:18:56 +0900

    [Bronze I] Title: 단어 공부, Time: 240 ms, Memory: 26420 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,48 +1,30 @@
 import java.io.*;
 import java.util.*;
-
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Map<String, Integer> map = new HashMap<String, Integer>();
-        for (int i = 65; i <= 90; i++) {
-        	map.put(String.valueOf((char) i), 0);
+    public static void main(String[] args) throws Exception{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String s =bf.readLine();
+        s = s.toUpperCase();
+        int []num = new int[26];
+        char []ch = s.toCharArray();
+        for(int k=0;k<s.length();k++){
+            char tmp = s.charAt(k);
+            for(char i=65;i<91;i++){
+                if(tmp==i){
+                    num[i-65]++;
+                }
+            }
         }
-        
-        String word = br.readLine();
-        word = word.toUpperCase();
-        String[] spells = new String[word.length()];
-        for (int i = 0; i < word.length(); i++) {
-        	spells[i] = String.valueOf(word.charAt(i));
+        int max=-1;
+        char mx='?';
+        for( int i=0;i<26;i++){ // num 배열의 최대값을 찾고
+            if(num[i]>max){     // 최대값일 때 마다 그때의 인덱스에 해당하는 문자를 mx에 저장
+                max = num[i];
+                mx = (char)(i+65);
+            }else if(num[i]==max){ // 최대값들과 동일한 num배열값이 존재한다면 mx를 ?로 저장
+                mx = '?';
+            }
         }
-        
-        for (int i = 0; i < spells.length; i++) {
-        	map.put(spells[i], map.get(spells[i]) + 1);
-        }
-        
-        int max = -1;
-        String maxKey = "";
-        
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-        	if (entry.getValue() > max) {
-        		max = entry.getValue();
-        		maxKey = entry.getKey();
-        	}
-        }
-        
-        int cnt = 0;
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-        	if (max == entry.getValue()) {
-        		cnt++;
-        	}
-        }
-        
-        if (cnt == 1) {
-        	System.out.println(maxKey);
-        }
-        else {
-        	System.out.println("?");
-        }
+        System.out.println(mx);
     }
 }
