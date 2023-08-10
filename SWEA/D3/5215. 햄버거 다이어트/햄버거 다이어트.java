5d07feafca4007caf867593d3bc21423b8e4859c
Hojo Choi <87483951+ubermensch100326@users.noreply.github.com>
2023-08-10 15:22:31 +0900

    [D3] Title: 햄버거 다이어트, Time: 965 ms, Memory: 20,556 KB -BaekjoonHub

---
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+	static int max = 0;
+	static int[] flavour;
+	static int[] calorie;
+	static int N = 0;
+	static int L = 0;
+	static int tempFlavour = 0;
+	static int tempCalorie = 0;
+	static int test = 0;
+	
+	public static void combination(int goal, int cnt, int start) {
+		// 재료를 goal개 고르는 함수
+		if (cnt == goal) {
+			// goal개 골랐을 때 tempCalorie가 L보다 작고 tempFlavour가 max보다 크면 갱신
+			if (tempCalorie <= L) {
+				if (tempFlavour > max) {
+					max = tempFlavour;
+				}
+			}
+//			test++;
+//			System.out.println("Flavour : " + tempFlavour);
+//			System.out.println("Calorie : " + tempCalorie);
+//			System.out.println("------------------------");
+			return;
+		}
+		
+		// cnt번째 재료를 고르는 코드
+		for (int i = start; i < N; i++) {
+			
+			// 재료를 고르는 경우
+			tempFlavour += flavour[i];
+			tempCalorie += calorie[i];
+			combination(goal, cnt + 1, i + 1);
+			
+			// 재료를 고르지 않은 경우
+			tempFlavour -= flavour[i];
+			tempCalorie -= calorie[i];
+		}
+	}
+	
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		String[] srr = new String[2];
+		for (int t = 1; t <= T; t++) {
+			srr = br.readLine().split(" ");
+			N = Integer.parseInt(srr[0]);
+			L = Integer.parseInt(srr[1]);
+			
+			flavour = new int[N];
+			calorie = new int[N];
+			
+			for (int n = 0; n < N; n++) {
+				srr = br.readLine().split(" ");
+				flavour[n] = Integer.parseInt(srr[0]);
+				calorie[n] = Integer.parseInt(srr[1]);
+			}
+			
+			// 20개 재료 중에서 1개 고르는 경우, 2개 고르는 경우, ..., N 개 고르는 경우 고려
+			for (int i = 0; i <= N; i++) {
+				combination(i, 0, 0);
+			}
+//			combination(2, 0, 0);
+			
+			
+			System.out.printf("#%d %d\n", t, max);
+//			System.out.println(test);
+			max = 0;
+		}
+	}
+
+}
