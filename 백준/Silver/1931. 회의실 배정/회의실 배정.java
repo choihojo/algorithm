57a75dbe3d58eb7d1147b6abb85b160f2d7018d5
Hojo Choi <87483951+ubermensch100326@users.noreply.github.com>
2023-08-11 16:24:35 +0900

    [Silver I] Title: 회의실 배정, Time: 716 ms, Memory: 63352 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -13,6 +13,8 @@ public class Main {
 				list[n][i] = Integer.parseInt(srr[i]);
 			}
 		}
+//		끝나는 시간으로만 정렬하면 안됨
+//		끝나는 시간을 오름차순으로 정렬하고 만약 똑같다면 시작하는 시간도 오름차순으로 정렬해야함
 		Arrays.sort(list, (o1, o2) -> {
 			if (o1[1] == o2[1]) {
 				return o1[0] - o2[0];
@@ -22,11 +24,17 @@ public class Main {
 			}});
 		
 		List<Integer> idx = new ArrayList<>();
+		
+//		정렬한 뒤 첫 번째 회의 선택함
 		idx.add(0);
 		int j = 0;
+//		선택한 회의의 종료시간보다 빠른 시작시간을 가지는 회의는 무시함
+//		선택한 회의의 종료시간과 시작시간이 같거나 늦은 회의를 선택함
 		for (int i = 1; i < N; i++) {
 			if (list[j][1] <= list[i][0]) {
 				idx.add(i);
+//				새로운 회의 선택함
+//				S_ij = a_m U S_mj
 				j = i;
 			}
 		}
