c7c67756612102ac4af7cc3facd5b07283bd7614
Hojo Choi <87483951+ubermensch100326@users.noreply.github.com>
2023-08-15 23:48:46 +0900

    [Gold V] Title: 토마토, Time: 768 ms, Memory: 121920 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,136 +1,103 @@
 import java.io.*;
 import java.util.*;
 
-// for문을 이용해서 풀었을 땐 시간 초과가 나서 통과하지 못했음
-// 시간복잡도를 짐작하고 미리 반복문을 써도 될지 아니면 다른 방법을 강구해야될지 알 수 있어야할 듯
-// 1인 모든 지점에서 동시에(같은 day) bfs로 1칸씩 가야함
+// 설명은 이전 풀이에 자세하게 있음
+// 이번엔 Data 클래스를 따로 만들고 최단경로 알고리즘을 이용하여 풀어봤음
 public class Main {
-	static String[] srr;
-	static BufferedReader br;
+	static int[][] map;
 	static int M;
 	static int N;
-	static int[][] map;
-//	방문체크 배열 (이미 주위 상하좌우로 익게 한 토마토의 경우 살펴볼 필요가 없음)
-//	근데 어차피 전체 토마토를 탐색하는 것이 아니라 1인 토마토를 기준으로 상하좌우로 0인 것만 넣고 빼고하는 거라 큰 차이는 없는 듯함
-//	실제로 bMap 관련 코드를 빼고 돌려도 통과함
-	static boolean[][] bMap;
-	static int cnt;
-	static int total;
 	static int day;
 	static int[] dRow = new int[] {0, -1, 0, 1};
 	static int[] dCol = new int[] {1, 0, -1, 0};
-	static List<int[]> start;
+	static boolean flag;
 	
-	public static boolean bfsByQueue(List<int[]> start) {
-//		이렇게 배열을 큐에 넣는 방법보다 객체화해서 넣는 방법도 해볼 필요가 있음
-//		이 문제처럼 저장해야되는 타입이 한가지면 배열로 넣어도 무방하지만 여러 타입을 저장해야 한다면 객체화하는 게 편할 수 있음
-//		예를 들어 Data 클래스를 만들고 row 프로퍼티와 col 프로퍼티를 만들어서 쓰는 거임
-		Queue<int[]> queue = new ArrayDeque<>();
-		int[] poll;
-		int row;
-		int col;
-		int pRow;
-		int pCol;
-		int size;
-		boolean flag;
+	public static void bfsByQueue() {
+		Queue<Data> queue = new ArrayDeque<>();
 		
-		for (int i = 0; i < start.size(); i++) {
-			int tempRow = start.get(i)[0];
-			int tempCol = start.get(i)[1];
-//			시작점들은 poll하고 4방위를 고려하는 게 확정이므로 미리 여기서 방문체크해줌
-//			이 문제에서는 방문체크 자체가 답에 영향을 주진 않음
-			bMap[tempRow][tempCol] = true;
-			queue.offer(new int[] {tempRow, tempCol});
-		}
-		
-//		while문에서 탈출하면 모든 익을 수 있는 토마토는 익은 상태임
-		while (!queue.isEmpty()) {
-//			익은 토마토 개수가 변했는지 체크하기 위한 변수
-//			만약 이걸 안 넣으면 목표 값보다 +1된 값이 나옴
-//			offer 없이 단순히 poll만 하는(익은 토마토 개수 변화X) 경우에도 day++가 이루어지기 때문임
-			flag = false;
-			
-//			큐의 사이즈를 재는 것이 곧 day를 계산하는 것이고 이 문제의 핵심임
-//			지금 큐의 사이즈만큼 poll하고 그에 따른 4방위 요소들을 넣어줘야 함
-//			현재 큐에 (1일 1일 1일) 이렇게 들어있다고 할 때 (2일 2일 2일, ..., 2일) 이렇게 될 때까지 빼주는 것임
-//			근데 이렇게 사이즈를 이용하지 않고 기존 최단경로 알고리즘처럼 이전 칸수에서 1 더한 값을 다음 칸수에 저장해도 day 계산이 가능해보임
-//			Data 클래스와 최단경로 알고리즘으로 다시 풀어볼 예정
-			size = queue.size();
-			
-			for (int s = 0; s < size; s++) {
-				poll = queue.poll();
-				pRow = poll[0];
-				pCol = poll[1];
-				
-				for (int i = 0; i < 4; i++) {
-					row = pRow + dRow[i];
-					col = pCol + dCol[i];
-					if (row >= 0 && row < N && col >= 0 && col < M) {
-						if (!bMap[row][col]) {
-//							여기까지 조건을 통과했으면 poll한 노드의 4방위 이동 후 노드를 방문했다는 것이므로 체크
-//							-1을 방문체크해놓으면 다음번에 애초에 들어올 필요도 없음
-//							0이어도 아래에서 1로 바꾸고 큐에 넣어주는 순간 역할을 다한 거임
-//							값이 1인 좌표는 애초에 이 조건문에 들어올 수 없음 (시작할 때의 1들은 이미 방문체크를 해놨었음)
-//							나중에 익어서 생기는 1은 아래 조건문을 통과한 0에서 바뀐 1들인데 그 전에 방문체크를 하는 코드가 있기 때문임
-							bMap[row][col] = true;
-							if (map[row][col] == 0) {
-								map[row][col] = 1;
-								queue.offer(new int[] {row, col});
-								cnt++;
-								flag = true;
-							}
-						}
-					}
+//		처음 시작할 때 이미 익어있던 토마토들을 큐에 넣음
+		for (int n = 0; n < N; n++) {
+			for (int m = 0; m < M; m++) {
+				if (map[n][m] == 1) {
+					queue.add(new Data(n, m));
 				}
 			}
+		}
+		
+		Data poll;
+		int pRow;
+		int pCol;
+		int row;
+		int col;
+		
+		while (!queue.isEmpty()) {
+			poll = queue.poll();
+			pRow = poll.row;
+			pCol = poll.col;
 			
-			if (flag) {
-				day++;	
+			for (int i = 0; i < 4; i++) {
+				row = pRow + dRow[i];
+				col = pCol + dCol[i];
+//				배열 인덱스 초과 방지
+				if (row >= 0 && row < N && col >= 0 && col < M) {
+//					만약 한 칸 이동한 좌표의 값이 0(익지 않은 토마토)일 때만 이전 좌표에서 1을 더한 값으로 갱신함
+//					이 조건문 덕분에 별도로 방문체크 배열이 필요없음
+					if (map[row][col] == 0) {
+						map[row][col] = map[pRow][pCol] + 1;
+						queue.offer(new Data(row, col));
+					}				
+				}
 			}
 		}
-//		이 반환문 하나면 처음부터 익은 토마토가 하나도 없는 경우, 모두 익었을 경우 등 모두 고려 가능함
-		return cnt == total;
 	}
-
+	
 	public static void main(String[] args) throws Exception {
-		br = new BufferedReader(new InputStreamReader(System.in));
-		srr = br.readLine().split(" ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] srr = br.readLine().split(" ");
 		M = Integer.parseInt(srr[0]);
 		N = Integer.parseInt(srr[1]);
 		map = new int[N][M];
-//		bMap이 핵심인데 아래 루프문 설명 참고
-		bMap = new boolean[N][M];
-		start = new ArrayList<>();
-		
-//		며칠이 지났는지 확인하기 위한 변수
-		day = 0;
-//		익지 않은 토마토의 개수
-		total = 0;
-//		현재까지 몇 개를 익게 했는지 알려주는 변수
-		cnt = 0;
-		
 		for (int n = 0; n < N; n++) {
 			srr = br.readLine().split(" ");
 			for (int m = 0; m < M; m++) {
 				map[n][m] = Integer.parseInt(srr[m]);
-//				익게 해야되는 토마토의 개수 카운팅 (목표 개수)
+			}
+		}
+		
+		bfsByQueue();
+		
+		flag = true;
+		day = 0;
+		for (int n = 0; n < N; n++) {
+			for (int m = 0; m < M; m++) {
+//				단 하나라도 0이 남아있다면 토마토가 다 익지 못했다는 것임
 				if (map[n][m] == 0) {
-					total++;
+					flag = false;
 				}
-//				처음 시작할 때 익은 토마토의 좌표를 리스트에 추가
-				else if (map[n][m] == 1) {
-					start.add(new int[] {n, m});
+//				처음 시작이 1이므로 총 익는 데 걸린 날은 map의 최댓값에서 1을 뺀 값임
+//				어떤 좌표의 값에서 1을 빼면 그 좌표의 토마토가 익을 때까지 걸린 날임
+				if (map[n][m] > day) {
+					day = map[n][m] - 1;
 				}
 			}
 		}
 		
-//		bfsByQueue가 참이면 토마토를 모두 익게 할 수 있다는 것이고 day 출력
-//		그렇지 않을 경우 -1 출력
-		if (bfsByQueue(start)) {
+		if (flag) {
 			System.out.println(day);
 		}
 		else {
 			System.out.println(-1);
 		}
+		
+	}
+}
+
+class Data {
+	public int row;
+	public int col;
+	
+	Data(int row, int col){
+		this.row = row;
+		this.col = col;
 	}
 }
