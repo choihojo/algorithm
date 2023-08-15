27e96b20f730bf44850cc5cb96ad0b5720f15a10
Hojo Choi <87483951+ubermensch100326@users.noreply.github.com>
2023-08-15 16:23:58 +0900

    [Silver I] Title: 단지번호붙이기, Time: 84 ms, Memory: 11868 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -33,6 +33,7 @@ public class Main {
 //			1칸 나아간 인덱스가 전체 인덱스 범위를 초과하는지 확인
 			if (row >= 0 && row < N && col >= 0 && col < N) {
 //				만약 그 곳에 방문한 적 없고 갈 수 있는 곳(1)이라면 조건문에 진입
+//				굳이 방문체크 배열을 쓰지 않고 지나갔던 곳을 0으로 바꾸는 방식으로도 가능함
 				if (!visited[row][col] && map[row][col] == 1) {
 //					방문 체크
 					visited[row][col] = true;
@@ -120,39 +121,40 @@ public class Main {
 		for (int i = 0; i < N; i++) {
 			str = br.readLine();
 			for (int j = 0; j < N; j++) {
+//				이렇게 말고 map[i][j] = str.charAt(j) - '0';이 더 깔끔한 듯
 				map[i][j] = Integer.parseInt(String.valueOf(str.charAt(j)));
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (!visited[i][j] && map[i][j] == 1) {
+//					우선 방문되지 않은 곳 중에서 1인 곳이 있었다는 건 거길 시작으로 하는 단지가 존재한다는 뜻임
+					cnt++;
+//					최소 집의 수가 1개는 된다는 것이므로 houseCnt 1로 초기화
+					houseCnt = 1;
+//					방문했다고 판정
+					visited[i][j] = true;
+//					dfsByRecursion으로 단지 탐색
+					dfsByRecursion(new int[] {i, j});
+//					N번째 단지의 집의 수는 (N - 1)번째에 저장됨 (0번째부터 시작하므로)
+					houseList[cnt - 1] = houseCnt;
+				}
 			}
 		}
 		
 //		for (int i = 0; i < N; i++) {
 //			for (int j = 0; j < N; j++) {
 //				if (!visited[i][j] && map[i][j] == 1) {
-////					우선 방문되지 않은 곳 중에서 1인 곳이 있었다는 건 거길 시작으로 하는 단지가 존재한다는 뜻임
 //					cnt++;
-////					최소 집의 수가 1개는 된다는 것이므로 houseCnt 1로 초기화
 //					houseCnt = 1;
-////					방문했다고 판정
 //					visited[i][j] = true;
-////					dfsByRecursion으로 단지 탐색
-//					dfsByRecursion(new int[] {i, j});
-////					N번째 단지의 집의 수는 (N - 1)번째에 저장됨 (0번째부터 시작하므로)
+////					dfsByStack으로 단지 탐색
+//					dfsByStack(new int[] {i, j});
 //					houseList[cnt - 1] = houseCnt;
 //				}
 //			}
 //		}
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (!visited[i][j] && map[i][j] == 1) {
-					cnt++;
-					houseCnt = 1;
-					visited[i][j] = true;
-//					dfsByStack으로 단지 탐색
-					dfsByStack(new int[] {i, j});
-					houseList[cnt - 1] = houseCnt;
-				}
-			}
-		}
 		
 //		for (int i = 0; i < N; i++) {
 //			for (int j = 0; j < N; j++) {
