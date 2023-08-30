cb90ee38d0820dd5184450a2c7cba314bced45c0
Hojo Choi <87483951+ubermensch100326@users.noreply.github.com>
2023-08-30 15:34:14 +0900

    [Bronze V] Title: A+B - 7, Time: 124 ms, Memory: 12672 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int T;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		T = Integer.parseInt(st.nextToken());
+		for (int t = 1; t <= T; t++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			System.out.printf("Case #%d: %d\n", t, a + b);
+		}
+	}
+}
