108c4b3e8af59b8bb33ba965a6ee0a387eee7633
Hojo Choi <87483951+ubermensch100326@users.noreply.github.com>
2023-10-05 22:14:15 +0900

    [Gold IV] Title: 키 순서, Time: 1024 ms, Memory: 36272 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -1,59 +1,63 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N,M,arr[][];
-	static int tCnt, lCnt;
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N+1][N+1];
-		
-		for(int i = 0; i < M; i++) {
-			st = new StringTokenizer(bf.readLine());
-			int low = Integer.parseInt(st.nextToken());
-			int tall = Integer.parseInt(st.nextToken());
-			arr[low][tall] = 1;
-		}
-		
-		int count = 0;
-		
-		for(int i = 1; i <= N; i++) {
-			lCnt = 0;
-			tCnt = 0;
-			lDfs(i, new boolean[N + 1]);
-			tDfs(i, new boolean[N + 1]);
-			if(lCnt + tCnt == N - 1) {
+	static BufferedReader br;
+	static StringTokenizer st;
+	static int N, M;
+	static boolean[] visited;
+	static int[][] adjMatrix;
+	static int count;
+	static int result;
+	
+	static int lDfs(int start) {
+		for (int i = 1; i <= N; i++) {
+			if (adjMatrix[start][i] == 0) continue;
+			if (visited[i]) continue;
+			else {
+				visited[i] = true;
 				count++;
+				lDfs(i);
 			}
 		}
-		
-		System.out.println(count);
+		return 0;
 	}
 	
-	static void tDfs(int cur, boolean v[]) {
-		v[cur] = true;
-		for(int i = 1; i <= N; i++) {
-			if(!v[i] && arr[cur][i] == 1) {
-				tCnt++;
-				tDfs(i, v);
+	static int gDfs(int end) {
+		for (int i = 1; i <= N; i++) {
+			if (adjMatrix[i][end] == 0) continue;
+			if (visited[i]) continue;
+			else {
+				visited[i] = true;
+				count++;
+				gDfs(i);
 			}
 		}
+		return 0;
 	}
-
-	static void lDfs(int cur, boolean v[]) {
-		v[cur] = true;
-		for(int i = 1; i <= N; i++) {
-			if(!v[i] && arr[i][cur] == 1) {
-				lCnt++;
-				lDfs(i, v);
-			}
+	
+	public static void main (String[] args) throws Exception {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		visited = new boolean[N + 1];
+		adjMatrix = new int[N + 1][N + 1];
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			adjMatrix[a][b] = 1;
 		}
+		for (int i = 1; i <= N; i++) {
+			lDfs(i);
+			gDfs(i);
+			if (count == (N - 1)) result++;
+			Arrays.fill(visited, false);
+			count = 0;
+		}
+		System.out.println(result);
 	}
 }
