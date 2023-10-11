7bb2cba709b6d638892e74e2efb6931501515295
Hojo Choi <87483951+ubermensch100326@users.noreply.github.com>
2023-10-12 00:56:46 +0900

    [Gold IV] Title: 웨이팅, Time: 624 ms, Memory: 48308 KB -BaekjoonHub

---
+++ 백준/Gold/30054. 웨이팅/웨이팅.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	static BufferedReader br;
+	static StringTokenizer st;
+	static boolean[] reserved;
+	static boolean[] visited;
+	static int[][] guests;
+	static int[] arrived;
+	static int N;
+	static int waiting() {
+		Arrays.sort(guests, (o1, o2) -> (o1[1] > o2[1] ? 1 : (o1[1] == o2[1] ? 0 : -1)));
+		int max = -1;
+		int idx = 0;
+		int waitTime = 0;
+		int cnt = 0;
+		for (int t = 1; t <= 200_000; t++) {
+			if (reserved[t]) {
+				if (!visited[t]) {
+					visited[t] = true;
+					waitTime = (t - arrived[t]);
+					if (max < waitTime) max = waitTime;
+					cnt++;
+				}
+				else {
+					while (visited[guests[idx][0]]) {
+						idx++;
+					}
+					if (t < guests[idx][1]) continue;
+					visited[guests[idx][0]] = true;
+					waitTime = (t - guests[idx][1]);
+					if (max < waitTime) max = waitTime;
+					cnt++;
+				}
+			}
+			else {
+				while (visited[guests[idx][0]]) {
+					idx++;
+				}
+				if (t < guests[idx][1]) continue;
+				visited[guests[idx][0]] = true;
+				waitTime = (t - guests[idx][1]);
+				if (max < waitTime) max = waitTime;
+				cnt++;
+			}
+			if (cnt == N) return max;
+		}
+		for (int t = 200_001; t <= 300_000; t++) {
+			while (visited[guests[idx][0]]) {
+				idx++;
+			}
+			if (t < guests[idx][1]) continue;
+			visited[guests[idx][0]] = true;
+			waitTime = (t - guests[idx][1]);
+			if (max < waitTime) max = waitTime;
+			cnt++;
+			if (cnt == N) return max;
+		}
+		return max;
+	}
+	public static void main(String[] args) throws Exception {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		reserved = new boolean[200_001];
+		visited = new boolean[200_001];
+		guests = new int[N][2];
+		arrived = new int[200_001];
+		for (int n = 0; n < N; n++) {
+			st = new StringTokenizer(br.readLine());
+			int reserveTime = Integer.parseInt(st.nextToken());
+			int arriveTime = Integer.parseInt(st.nextToken());
+			guests[n][0] = reserveTime;
+			guests[n][1] = arriveTime;
+			arrived[reserveTime] = arriveTime;
+			if (reserveTime >= arriveTime) reserved[reserveTime] = true;
+		}
+		System.out.println(waiting());
+	}
+}
