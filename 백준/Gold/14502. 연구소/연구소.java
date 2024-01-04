37c31009a883e15e18c60410748d58cf647b54b4
Choi Hojo <ubermensch100326@gmail.com>
2024-01-04 21:59:28 +0900

    [Gold IV] Title: 연구소, Time: 708 ms, Memory: 296380 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,110 +1,118 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
+/*
+ * N : 세로 크기 (row)
+ * M : 가로 크기 (col)
+ * 
+ * 0 : 빈 칸
+ * 1 : 벽
+ * 2 : 바이러스
+ * 
+ * -> 벽을 3개 세우고 안전지역의 최대 크기?
+*/
 
 public class Main {
-	static int N, M,safty,map[][];
-	static boolean[] v;
-	static boolean[][] v2;
-	static int res[] = new int[3];
-	static int min = Integer.MAX_VALUE;
-	static ArrayList<int[]> b = new ArrayList<>();
-	static ArrayList<int[]> s = new ArrayList<>();
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
+	static BufferedReader br;
+	static StringTokenizer st;
+	static int N, M;
+	static int[][] map;
+	static int max = Integer.MIN_VALUE;
+	static int[] dRow = new int[] {0, -1, 0, 1};
+	static int[] dCol = new int[] {1, 0, -1, 0};
+	
+	static void dfs(int count) {
+		if (count == 3) {
+			int temp = calculate(bfs());
+			if (max < temp) {
+				max = temp;
+			}
+			return;
+		}
+		
+		for (int n = 0; n < N; n++) {
+			for (int m = 0; m < M; m++) {
+				if (map[n][m] == 0) {
+					map[n][m] = 1;
+					dfs(count + 1);
+					map[n][m] = 0;
+				}
+			}
+		}
+	}
+	
+	static int[][] bfs() {
+		Queue<int[]> queue = new ArrayDeque<>();
+		
+		int[][] clone = new int[N][M];
+		for (int n = 0; n < N; n++) {
+			for (int m = 0; m < M; m++) {
+				clone[n][m] = map[n][m];
+				if (clone[n][m] == 2) {
+					queue.offer(new int[] {n, m});
+				}
+			}
+		}
+		
+		int[] poll = null;
+		int pRow = 0;
+		int pCol = 0;
+		int row = 0;
+		int col = 0;
+		while (!queue.isEmpty()) {
+			poll = queue.poll();
+			pRow = poll[0];
+			pCol = poll[1];
+			
+			for (int i = 0; i < 4; i++) {
+				row = pRow + dRow[i];
+				col = pCol + dCol[i];
+				if (row >= 0 && row < N && col >= 0 && col < M) {
+					if (clone[row][col] == 0) {
+						clone[row][col] = 2;
+						queue.offer(new int[] {row, col});
+					}
+				}
+			}
+		}
+		
+		return clone;
+	}
+	
+	static int calculate(int[][] clone) {
+		int size = 0;
+		
+		for (int n = 0; n < N; n++) {
+			for (int m = 0; m < M; m++) {
+				if (clone[n][m] == 0) {
+					size++;
+				}
+			}
+		}
+		
+		return size;
+	}
+	
+	
+	public static void main(String[] args) throws Exception {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		
 		map = new int[N][M];
-		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(bf.readLine());
-			for(int j = 0; j < M; j++) {
-				int temp = Integer.parseInt(st.nextToken());
-				map[i][j] = temp;
-				if(temp == 0) {
-					s.add(new int[] {i,j});
-				}else if(temp == 2) {
-					b.add(new int[] {i,j});
-				}
+		
+		for (int n = 0; n < N; n++) {
+			st = new StringTokenizer(br.readLine());
+			for (int m = 0; m < M; m++) {
+				map[n][m] = Integer.parseInt(st.nextToken());
 			}
 		}
-		safty = s.size();
-		v = new boolean[safty];
 		
-		combi(0,0);
+		dfs(0);
 		
-		System.out.println(safty-min-3);
+		System.out.println(max);
 	}
-	
-	static void combi(int cnt, int start) {
-		if(cnt == 3) {
-			toDo();
-			return;
-		}
-		for(int i = start; i < safty; i++) {
-			if(v[i]) {
-				continue;
-			}
-			v[i] = true;
-			res[cnt] = i;
-			combi(cnt + 1, i + 1);
-			v[i] = false;
-		}
-	}
-	
-	static void toDo() {
-		int temp[][] = new int[N][M];
-		for(int i = 0; i < N; i++) {
-			temp[i] = Arrays.copyOf(map[i], M);
-		}
-		for(int i = 0; i < 3; i++) {
-			temp[s.get(res[i])[0]][s.get(res[i])[1]] = 1;
-		}
-		
-		v2 = new boolean[N][M];
-		
-		Queue<int[]> q = new ArrayDeque<>();
-		int cnt = 0;
-		for(int i = 0; i < b.size(); i++) {
-//			System.out.println(Arrays.toString(b.get(i)));
-			q.offer(b.get(i));
-		}
-		int dx[] = new int[] {-1,1,0,0};
-		int dy[] = new int[] {0,0,-1,1};
-		while(!q.isEmpty()) {
-//			for(int i = 0; i < N ; i++) {
-//				System.out.println(Arrays.toString(temp[i]));
-//			}
-//			System.out.println();
-			int tmp[] = q.poll();
-//			System.out.println(Arrays.toString(tmp));
-			int x = tmp[0];
-			int y = tmp[1];
-			for(int i = 0; i < 4; i++) {
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-//				System.out.println(nx + " " + ny);
-				if(nx >= 0 && nx < N && ny >= 0 && ny < M  && temp[nx][ny] == 0) {
-					cnt++;
-					temp[nx][ny] = 2;
-					q.offer(new int[] {nx,ny});
-				}
-			}
-			if(cnt > min) {
-				return;
-			}
-		}
-		
-		
-		if(cnt < min) {
-			min = cnt;
-		}
-	}
+
 }
