8d6cc17b3bc5623a0a5a76e7ae4f806161695c5b
Choi Hojo <choihojo@outlook.com>
2024-05-07 12:19:32 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10080 ms, Memory: 305000 KB -BaekjoonHub

---
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M;
+	static List<Integer>[] list;
+	static boolean[] visited;
+	static int[] counted;
+	static StringBuilder sb;
+	static int max;
+	
+	static void bfs(int i, int temp) {
+		Queue<Integer> queue = new ArrayDeque<>();
+		
+		queue.offer(i);
+		visited[i] = true;
+		int poll = 0;
+		
+		while (!queue.isEmpty()) {
+			poll = queue.poll();
+			for (int j : list[poll]) {
+				if (!visited[j]) {
+					queue.offer(j);
+					visited[j] = true;
+					counted[j]++;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		int temp = N + 1;
+		
+		list = new ArrayList[temp];
+		for (int i = 0; i < temp; i++) {
+			list[i] = new ArrayList<>();
+		}
+		
+		visited = new boolean[temp];
+		counted = new int[temp];
+		
+		for (int m = 0; m < M; m++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			list[a].add(b);
+		}
+		
+		for (int i = 1; i < temp; i++) {
+			visited = new boolean[N + 1];
+			bfs(i, temp);			
+		}
+		
+		for (int i = 1; i < temp; i++) {
+			max = max < counted[i] ? counted[i] : max;
+		}
+		sb = new StringBuilder();
+		for (int i = 1; i < temp; i++) {
+			if (counted[i] == max) {
+				sb.append(i).append(" ");
+			}
+		}
+		
+		System.out.println(sb);
+		
+	}
+	
+}
